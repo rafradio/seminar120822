2af34f70cc53516bbfb2c7d775323b2c0b3e82f7
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-15 17:28:27 +0300

    HW120822 Task21 Vector

---
+++ HW120822_Task21/AddClasses.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Vector
+{
+    class ExecMethod
+    {
+        public ExecMethod(InitialSetting obj, InitialSetting obj1)
+        {
+            Console.WriteLine("Второй ввод " + obj.XAxes + " " + obj1.YAxes);
+
+        }
+    }
+}
---
+++ HW120822_Task21/Vector21.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Vector
+{
+    public class Program 
+    {
+        public int firstNum = -10;
+        public int upperNum = 10;
+        public int randExecute()
+        {
+            int number = new Random().Next(this.firstNum, this.upperNum);
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Program initial = new Program();
+            InitialSetting aDot = new InitialSetting(initial);
+            InitialSetting bDot = new InitialSetting(initial);
+            Console.WriteLine(aDot.XAxes + " " + aDot.YAxes + " " + aDot.ZAxes);
+            Console.WriteLine(bDot.XAxes + " " + bDot.YAxes + " " + bDot.ZAxes);
+            ExecMethod execVector = new ExecMethod(aDot, bDot);
+
+        }
+    }
+    public class InitialSetting 
+    {
+        public int XAxes;
+        public int YAxes;
+        public int ZAxes;
+        public InitialSetting(Program obj2)
+        {
+            XAxes = obj2.randExecute();
+            YAxes = obj2.randExecute();
+            ZAxes = obj2.randExecute();
+        }
+
+    }
+}
