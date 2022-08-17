65fe961b2c8d7c8d49a73eb0959b5adf058568f1
Rafail Abdyushev <rafaelab@mail.ru>
2022-08-17 11:00:30 +0300

    HW120822 Task21 Vector result

--- HW120822_Task21/AddClasses.cs
+++ HW120822_Task21/AddClasses.cs
@@ -4,9 +4,15 @@ namespace Vector
 {
     class ExecMethod
     {
+        public double LengthVector;
         public ExecMethod(InitialSetting obj, InitialSetting obj1)
         {
-            Console.WriteLine("Второй ввод " + obj.XAxes + " " + obj1.YAxes);
+            int sum = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                sum += Convert.ToInt32(Math.Pow((obj.CoorAxes[i] - obj1.CoorAxes[i]), 2));
+            }
+            LengthVector = Math.Sqrt(sum);
 
         }
     }
--- HW120822_Task21/Vector21.cs
+++ HW120822_Task21/Vector21.cs
@@ -5,7 +5,7 @@ namespace Vector
     public class Program 
     {
         public int firstNum = -10;
-        public int upperNum = 10;
+        public int upperNum = 11;
         public int randExecute()
         {
             int number = new Random().Next(this.firstNum, this.upperNum);
@@ -17,22 +17,22 @@ namespace Vector
             Program initial = new Program();
             InitialSetting aDot = new InitialSetting(initial);
             InitialSetting bDot = new InitialSetting(initial);
-            Console.WriteLine(aDot.XAxes + " " + aDot.YAxes + " " + aDot.ZAxes);
-            Console.WriteLine(bDot.XAxes + " " + bDot.YAxes + " " + bDot.ZAxes);
+            Console.WriteLine(aDot.CoorAxes[0] + " " + aDot.CoorAxes[1] + " " + aDot.CoorAxes[2]);
+            Console.WriteLine(bDot.CoorAxes[0] + " " + bDot.CoorAxes[1] + " " + bDot.CoorAxes[2]);
             ExecMethod execVector = new ExecMethod(aDot, bDot);
+            Console.WriteLine($"Расстояние между двумя точками в 3D пространстве равно {execVector.LengthVector}");
 
         }
     }
     public class InitialSetting 
     {
-        public int XAxes;
-        public int YAxes;
-        public int ZAxes;
+        public int[] CoorAxes = new int[4];
         public InitialSetting(Program obj2)
         {
-            XAxes = obj2.randExecute();
-            YAxes = obj2.randExecute();
-            ZAxes = obj2.randExecute();
+            for (int i = 0; i < 3; i++) 
+            {
+                CoorAxes[i] = obj2.randExecute();
+            }
         }
 
     }
